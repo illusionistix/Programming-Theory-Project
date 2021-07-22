1a74b44225abd73758dbe496ceb82d3e4eadab0a
illusionistix <86018686+illusionistix@users.noreply.github.com>
2021-07-22 22:04:25 +0200

    title screen added
    Created a startmenu scene with a title screen canvas containing a title text and a button.
    
    Removed the MainManager empty containing the MM script from the game scene and created it new in the startmenu scene (now scene 0).
    
    Added a public StartGame method to the MainManager script. This is called when the new button is clicked and loads the game scene (scene 1).
    
    Fixed some minor RBD issues.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour
 {
@@ -17,6 +18,11 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 
 }
